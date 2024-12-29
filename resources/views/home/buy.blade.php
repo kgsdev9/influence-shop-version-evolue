eb13e5ba5c12ffc04b422e89b6714dc230845ac1
kgsdev9 <kgsdev8@gmail.com>
2024-12-29 16:29:20 +0100

    add-classe-to-select-methode-payment

--- resources/views/home/buy.blade.php
+++ resources/views/home/buy.blade.php
@@ -116,49 +116,51 @@
                                 <div class="row">
                                     <!-- Boucle pour les inputs de fichier -->
                                     <template x-for="(file, index) in adressepyament" :key="index">
-
                                         <div class="col-lg-4 col-12 mb-4">
                                             <!-- form -->
-                                            <div class="border p-4 rounded-3">
+                                            <div class="border p-4 rounded-3" 
+                                                 :class="selectedAdresse?.id === file.id ? 'border-success bg-light' : ''">
                                                 <div class="form-check mb-2">
                                                     <input class="form-check-input" type="radio" name="flexRadioDefault"
-                                                        :id="'homeRadio' + index" />
-                                                    <label class="form-check-label text-dark fw-semibold"
-                                                        :for="'homeRadio' + index">
+                                                        :id="'homeRadio' + index"
+                                                        @click="selectAdresse(file)" />
+                                                    <label class="form-check-label text-dark fw-semibold" :for="'homeRadio' + index">
                                                         <span x-text="file.telephone || 'Home'"></span>
-                                                        <!-- Dynamically set type or label -->
                                                     </label>
                                                 </div>
                                                 <!-- address -->
                                                 <p class="mb-0">
-                                                    <span x-text="file.city || 'N/A'"></span><br>
-                                                    <br>
+                                                    <span x-text="file.city || 'N/A'"></span><br><br>
                                                     <span x-text="file.adresse || 'N/A'"></span><br>
-
                                                     United States
                                                 </p>
-
+                                
                                                 <div class="d-flex justify-content-between mt-3">
-                                                    <button class="btn btn-warning btn-sm" @click="openModal(file)"><svg
-                                                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                            fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                                                            <path
-                                                                d="M12.146 0.854a2 2 0 0 1 2.828 2.828L4.5 12.5a1 1 0 0 1-.351.211l-3.5 1a1 1 0 0 1-1.21-1.21l1-3.5a1 1 0 0 1 .211-.351L11.292 2.026a2 2 0 0 1 .854-.854zM11.793 2.5l-9.192 9.193-.548 1.925 1.924-.548 9.192-9.192a1 1 0 0 0-1.415-1.415z" />
+                                                    <button class="btn btn-warning btn-sm" @click="openModal(file)">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                                            <path d="M12.146 0.854a2 2 0 0 1 2.828 2.828L4.5 12.5a1 1 0 0 1-.351.211l-3.5 1a1 1 0 0 1-1.21-1.21l1-3.5a1 1 0 0 1 .211-.351L11.292 2.026a2 2 0 0 1 .854-.854zM11.793 2.5l-9.192 9.193-.548 1.925 1.924-.548 9.192-9.192a1 1 0 0 0-1.415-1.415z" />
                                                         </svg>
                                                     </button>
                                                     <button class="btn btn-danger btn-sm" @click="deleteAdresse(file.id)">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                            height="16" fill="currentColor" class="bi bi-dash-circle"
-                                                            viewBox="0 0 16 16">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
                                                             <path d="M8 0a8 8 0 1 0 8 8A8 8 0 0 0 8 0zM7 4h2v8H7V4z"></path>
                                                         </svg>
                                                     </button>
                                                 </div>
                                             </div>
                                         </div>
-
                                     </template>
                                 </div>
+
+                                <div class="mt-3">
+                                    <h5>Adresse Sélectionnée :</h5>
+                                    <p>Ville : <span x-text="selectedAdresse?.city || 'Non sélectionnée'"></span></p>
+                                    <p>Adresse : <span x-text="selectedAdresse?.adresse || 'Non sélectionnée'"></span></p>
+                                    <p>Téléphone : <span x-text="selectedAdresse?.telephone || 'Non sélectionnée'"></span>
+                                    </p>
+                                </div>
+
+
 
                                 <hr>
                                 <h6 class="mb-3">Mode de paiement :</h6>
@@ -376,6 +378,8 @@
                 files: [],
                 pricedelivery: 0,
                 showModal: false,
+                selectedAdresse: null,
+
                 quantite: 1,
                 form: {
                     country_origine: '',
@@ -390,7 +394,9 @@
                 searchQuery: '',
                 currentAdresse: '',
 
-
+                selectAdresse(file) {
+                    this.selectedAdresse = file;
+                },
                 // Validation des champs obligatoires
                 validateStep() {
                     this.errors = {};
@@ -412,16 +418,13 @@
                     });
                 },
 
-              
+
                 calculateTTC() {
                     const priceVente = Number(this.product.price_vente) || 0;
                     const priceDelivery = Number(this.pricedelivery) || 0;
                     const quantity = Number(this.quantite) || 1; // Par défaut, quantité = 1 si elle est invalide
                     return ((priceVente * quantity) + priceDelivery).toFixed(2);
                 },
-
-
-
 
                 addAdresse() {
                     if (this.files.length >= 2) {
