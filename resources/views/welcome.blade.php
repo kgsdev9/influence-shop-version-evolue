8013afab1b545abc734ad9ef1c36a1c826946cff
kgsdev9 <kgsdev8@gmail.com>
2025-01-22 22:40:07 +0100

    lazy-loader-implementer-welcome-page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         style="background-image: url(../assets/images/mentor/mentor-glow.svg); background-repeat: no-repeat; background-size: contain">
         <div class="container bg-white rounded-4 pe-lg-0 overflow-hidden">
             <div class="row align-items-center gy-5 gy-xl-0">
-                <div class="col-lg-6 col-12" >
+                <div class="col-lg-6 col-12">
                     <div class="d-flex flex-column gap-4 px-lg-6 p-3">
                         <div class="d-flex flex-column gap-3">
                             <h1 class="mb-0 display-4 fw-bold">Boostez votre visibilité avec Influence Shop</h1>
@@ -15,18 +15,20 @@
                         </div>
                         <form>
                             <div class="input-group shadow">
-                                <label for="productSearch" class="form-label visually-hidden">Rechercher un produit</label>
+                                <label for="productSearch" class="form-label visually-hidden">Rechercher un
+                                    produit</label>
                                 <input type="text" class="form-control rounded-start-3" id="productSearch"
                                     name="productSearch" placeholder="Quel produit souhaitez-vous promouvoir ?"
-                                    aria-label="Quel produit souhaitez-vous promouvoir ?" aria-describedby="searchButton"
-                                    required="" x-model="searchQuery">
+                                    aria-label="Quel produit souhaitez-vous promouvoir ?"
+                                    aria-describedby="searchButton" required="" x-model="searchQuery">
                                 <button class="btn btn-warning" id="searchButton" type="submit">Rechercher</button>
                             </div>
                         </form>
                         <div class="d-flex flex-row gap-1 flex-wrap">
                             <!-- Affichage dynamique des catégories -->
-                            <template x-for="category in categories" :key="category.id">
-                                <a href="#!" class="btn btn-tag" @click.prevent="searchQuery = category.name" x-text="category.name"></a>
+                            <template x-for="category in categories" :key="category . id">
+                                <a href="#!" class="btn btn-tag" @click.prevent="searchQuery = category.name"
+                                    x-text="category.name"></a>
                             </template>
                         </div>
                     </div>
@@ -44,6 +46,131 @@
         </div>
 
     </section>
+
+    <section class="py-xl-8 py-6">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-10 col-md-10 col-12 mx-auto">
+                    <div class="d-flex flex-column gap-2 text-center mb-xl-7 mb-5">
+                        <h2 class="h1 mb-0">Produit incontournable de la semaine</h2>
+                        <p class="mb-0 px-xl-5">
+                            Explorez des opportunités passionnantes et faites partie d'une aventure marketing unique
+                            grâce
+                            à notre sélection de produits et campagnes innovants. Rejoignez-nous pour transformer votre
+                            influence en succès !
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Placeholders (lazy loader) -->
+            <div id="placeholder" class="row gy-4" x-show="isLoading">
+                <template x-for="n in 8" :key="n">
+                    <div class="col-md-3 mb-4">
+                        <div class="card shadow-sm border-light h-100">
+                            <div class="image-container bg-light" style="height: 200px;"></div>
+                            <div class="card-body">
+                                <h5 class="card-title bg-light text-light" style="height: 20px; width: 80%;"></h5>
+                                <p class="card-text bg-light text-light" style="height: 15px; width: 90%;"></p>
+                            </div>
+                        </div>
+                    </div>
+                </template>
+            </div>
+
+            <!-- Produits réels -->
+            <div class="row gy-4" x-show="!isLoading">
+                <template x-for="product in products" :key="product . id">
+                    <div class="col-md-3 mb-4">
+                        <div class="card shadow-sm border-light h-100">
+                            <!-- Image -->
+                            <div class="image-container" style="cursor: pointer; overflow: hidden; position: relative;">
+                                <img :src="product . images . length ? `/storage/${product . images[0] . imagename}` : '../../assets/images/default-product.jpg'" alt="Product Image"
+                                    class="card-img-top img-fluid rounded-top"
+                                    style="max-height: 200px; width: 100%; object-fit: contain;">
+                            </div>
+                            <!-- Informations produit -->
+                            <div class="card-body d-flex flex-column">
+                                <h5 class="card-title">
+                                    <a :href="`/product/${product . id}`" class="text-inherit"
+                                        x-text="product.name"></a>
+                                </h5>
+                                <p class="card-text text-muted"
+                                    x-text="product.description.length > 50 ? product.description.substring(0, 50) + '...' : product.description">
+                                </p>
+                                <div class="d-flex justify-content-between align-items-center mt-auto">
+                                    <h6 class="text-warning mb-0" x-text="product.price_vente"></h6>
+                                    <a :href="`/buy/${product . id}`" class="btn btn-danger btn-sm">
+                                        <i class="fe fe-shopping-cart fs-3"></i> Acheter
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </template>
+            </div>
+        </div>
+    </section>
+
+
+
+    {{-- <section class="py-xl-8 py-6">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-10 col-md-10 col-12 mx-auto">
+                    <div class="d-flex flex-column gap-2 text-center mb-xl-7 mb-5">
+                        <h2 class="h1 mb-0">Produit incontournable de la semaine</h2>
+                        <p class="mb-0 px-xl-5">Explorez des opportunités passionnantes et faites partie d'une aventure
+                            marketing unique grâce à notre sélection de produits et campagnes innovants. Rejoignez-nous
+                            pour
+                            transformer votre influence en succès !</p>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row gy-4">
+                <template x-for="product in products" :key="product . id">
+                    <div class="col-md-3 mb-4">
+                        <div class="card shadow-sm border-light h-100">
+                            <!-- Image avec gestion de zoom -->
+                            <div class="image-container" style="cursor: pointer; overflow: hidden; position: relative;">
+                                <img :src="product . images . length ? `/storage/${product . images[0] . imagename}` :
+        '../../assets/images/default-product.jpg'" alt="Product Image" @click="zoomed = !zoomed"
+                                    :class="zoomed ? 'zoomed' : ''" class="card-img-top img-fluid rounded-top"
+                                    style="max-height: 200px; width: 100%; object-fit: contain; transition: transform 0.3s ease;">
+                            </div>
+
+                            <!-- Informations produit -->
+                            <div class="card-body d-flex flex-column">
+                                <h5 class="card-title">
+                                    <a :href="`{{ route('product.show', '') }}/${product . id}`" class="text-inherit"
+                                        x-text="product.name"></a>
+                                </h5>
+                                <p class="card-text text-muted"
+                                    x-text="product.description.length > 50 ? product.description.substring(0, 50) + '...' : product.description">
+                                </p>
+                                <div class="d-flex justify-content-between align-items-center mt-auto">
+                                    <h6 class="text-warning mb-0" x-text="product.price_vente"></h6>
+                                    <a :href="`{{ route('buy.product', '') }}/${product . id}`"
+                                        class="btn btn-danger btn-sm"> <i class="fe fe-shopping-cart fs-3"></i>
+                                        Acheter</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </template>
+            </div>
+
+
+
+
+
+        </div>
+    </section> --}}
+
 
     <section class="py-6 py-lg-2 bg-white mt-2">
         <div class="container py-lg-6">
@@ -188,75 +315,38 @@
 
         </div>
     </section>
-
-
-    <section class="py-xl-8 py-6">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-10 col-md-10 col-12 mx-auto">
-                    <div class="d-flex flex-column gap-2 text-center mb-xl-7 mb-5">
-                        <h2 class="h1 mb-0">Produit incontournable de la semaine</h2>
-                        <p class="mb-0 px-xl-5">Explorez des opportunités passionnantes et faites partie d'une aventure
-                            marketing unique grâce à notre sélection de produits et campagnes innovants. Rejoignez-nous
-                            pour
-                            transformer votre influence en succès !</p>
-
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="row gy-4">
-                <template x-for="product in products" :key="product . id">
-                    <div class="col-md-3 mb-4">
-                        <div class="card shadow-sm border-light h-100">
-                            <!-- Image avec gestion de zoom -->
-                            <div class="image-container" style="cursor: pointer; overflow: hidden; position: relative;">
-                                <img :src="product . images . length ? `/storage/${product . images[0] . imagename}` :
-        '../../assets/images/default-product.jpg'" alt="Product Image"
-                                    @click="zoomed = !zoomed" :class="zoomed ? 'zoomed' : ''"
-                                    class="card-img-top img-fluid rounded-top"
-                                    style="max-height: 200px; width: 100%; object-fit: contain; transition: transform 0.3s ease;">
-                            </div>
-
-                            <!-- Informations produit -->
-                            <div class="card-body d-flex flex-column">
-                                <h5 class="card-title">
-                                    <a :href="`{{ route('product.show', '') }}/${product . id}`" class="text-inherit"
-                                        x-text="product.name"></a>
-                                </h5>
-                                <p class="card-text text-muted"
-                                    x-text="product.description.length > 50 ? product.description.substring(0, 50) + '...' : product.description">
-                                </p>
-                                <div class="d-flex justify-content-between align-items-center mt-auto">
-                                    <h6 class="text-warning mb-0" x-text="product.price_vente"></h6>
-                                    <a :href="`{{ route('buy.product', '') }}/${product . id}`"
-                                        class="btn btn-danger btn-sm"> <i class="fe fe-shopping-cart fs-3"></i>
-                                        Acheter</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </template>
-            </div>
-
-
-
-
-
-        </div>
-    </section>
 </main>
 
 @endsection
+{{-- @push('script')
+<script>
+    function productManager() {
+        return {
+            searchQuery: '',
+            categories: @json($categories),
+            products: @json($listeproduct),
+        };
+    }
+</script>
+@endpush --}}
+
+
 @push('script')
     <script>
         function productManager() {
             return {
                 searchQuery: '',
                 categories: @json($categories),
-                products: @json($listeproduct),
+                products: [],
+                isLoading: true, // Indique si les données sont en cours de chargement
+
+                init() {
+                    // Simule un délai pour charger les produits
+                    setTimeout(() => {
+                        this.products = @json($listeproduct);
+                        this.isLoading = false; // Le chargement est terminé, les produits sont affichés
+                    }, 2000); // 2 secondes
+                },
             };
         }
     </script>
